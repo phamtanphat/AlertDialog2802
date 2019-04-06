5da5085ca1c7cb21e32bfa469bda4f015e123ad2
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-06 18:25:08 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/alertdialog/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/alertdialog/MainActivity.java
@@ -1,13 +1,50 @@
 package khoapham.ptp.phamtanphat.alertdialog;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAlertDialog = findViewById(R.id.buttonAlertDialog);
+
+
+        btnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Bạn có muốn thoát ứng dụng hay không?");
+                builder.setMessage("Bạn xác nhận bên dưới");
+                builder.setIcon(R.mipmap.ic_launcher);
+                builder.setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.setNeutralButton("Huy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 }
