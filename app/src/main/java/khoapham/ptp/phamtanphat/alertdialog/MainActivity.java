7a8da1274ebf1fcb75546a3c6d18fabba1f1bd99
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-06 18:43:59 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/alertdialog/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/alertdialog/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,28 +24,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Bạn có muốn thoát ứng dụng hay không?");
-                builder.setMessage("Bạn xác nhận bên dưới");
-                builder.setIcon(R.mipmap.ic_launcher);
-                builder.setNegativeButton("Khong", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.setPositiveButton("Co", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-                builder.setNeutralButton("Huy", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
 
+                builder.setIcon(R.mipmap.ic_launcher);
+                builder.setCancelable(false);
+
+                final String mangmonhoc[] = {"Android","React native","ios","php","css"};
+
+
+                builder.setSingleChoiceItems(mangmonhoc, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int position) {
+                        Toast.makeText(MainActivity.this, mangmonhoc[position], Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
                     }
                 });
                 builder.show();
             }
         });
+    }
+    public void setAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Bạn có muốn thoát ứng dụng hay không?");
+        builder.setMessage("Bạn xác nhận bên dưới");
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setCancelable(false);
+
+        builder.setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNeutralButton("Huy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
     }
 }
